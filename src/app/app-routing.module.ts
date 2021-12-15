5124d0ddb29e1c50dc2f385752a1448f50feb676
evert2208 <65311634+evert2208@users.noreply.github.com>
2021-12-15 15:25:28 -0500

    commit uno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarteleraComponent } from './pages/cartelera/cartelera.component';
+import { CreacionComponent } from './pages/creacion/creacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cartelera', component: CarteleraComponent},
+  {path: 'creacion', component: CreacionComponent},
+  {path: '**', redirectTo: 'cartelera'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarteleraComponent } from './pages/cartelera/cartelera.component';
+import { CreacionComponent } from './pages/creacion/creacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarteleraComponent,
+    CreacionComponent
   ],
   imports: [
     BrowserModule,
